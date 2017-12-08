cb0b14ee5b94592ee1d9a67d1e7a1e5fdb3b5ae4
tanyagriego <tanya.griego@gmail.com>
2017-12-07 18:18:14 -0800

    Beginning work on functions for new user form

---
+++ public/js/newUser.js
@@ -0,0 +1,17 @@
+var newUser = {};
+
+// Function that hides new user form until "Join Now" button is clicked
+newUser.newUserContainer = function() {
+  $('.newUserContainer').('click', '.userInfo', function (){
+    $('.')
+  }),
+}
+
+// Function to confirm password
+function confirmUserPassword () {
+  if ("password" === "confirmPassword")
+  return
+} else {
+  return "Oops! Your passwords didn't match. Please try again."
+  // while loop?
+}
