7199cf14dafd273472d310ae16bd8a6782e750c0
Ivan Limongan <ivan.limongan@gmail.com>
2017-12-07 20:42:54 -0800

    unplash random progress

--- public/js/unsplash.js
+++ public/js/unsplash.js
@@ -2,3 +2,7 @@
 // Credit: https://source.unsplash.com/  |  https://unsplash.com/
 
 
+$('.backgroundRandom').click(function() {
+    let link = "https://source.unsplash.com/random";
+    let random = Math.floor(Math.random() * link);
+});
