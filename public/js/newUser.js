1232895300ebe56281e36e67db52b9178302bac6
tanyagriego <tanya.griego@gmail.com>
2017-12-07 19:29:23 -0800

    Created newUser.newUserContainer function to hide newUserForm

--- public/js/newUser.js
+++ public/js/newUser.js
@@ -2,17 +2,21 @@ var newUser = {};
 
 // Function that hides new user form until "Join Now" button is clicked
 newUser.newUserContainer = function() {
-  $('.newUserContainer').('click', '.userInfo', function (){
-    $('.')
-  }),
-}
+  $('#JoinButton').on('click', function (event){
+    event.preventDefault();
+    $('#newUserForm').show();
+  })
+};
 
 // Function to confirm password
-function confirmUserPassword () {
-  if ("password" === "confirmPassword")
-  return
-} else {
-  return "Oops! Your passwords didn't match. Please try again."
-  // while loop?
-}
-lalalal
+// function confirmUserPassword () {
+//   if ("password" === "confirmPassword")
+//   return
+// } else {
+//   return "Oops! Your passwords didn't match. Please try again."
+//   // while loop?
+// }
+
+$(document).ready(function(){
+newUser.newUserContainer();
+});
