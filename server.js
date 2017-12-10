3b3e927ffd4eb13db181e13358c7fe372a49994b
tanyagriego <tanya.griego@gmail.com>
2017-12-10 14:02:05 -0800

    Created database for users and for styles

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const constring = 'postgres://tanyagriego@localhost:5432/';
+const constring = 'postgres://tanyagriego@localhost:5432/landinguser';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
@@ -17,3 +17,30 @@ app.get('/', function(request, response) {
 app.listen(PORT, function() {
   console.log(`Listening on port:${PORT}`);
 });
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    users (
+      user_id SERIAL PRIMARY KEY,
+      first_name VARCHAR(255),
+      last_name VARCHAR(255),
+      username VARCHAR(255),
+      password VARCHAR(255)
+    );`
+  )
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    styles (
+      styles_id SERIAL PRIMARY KEY,
+      font VARCHAR(255),
+      font_color VARCHAR(255),
+      background_image VARCHAR(255),
+      color_overlay VARCHAR(255),
+      gradient_overlay VARCHAR(255),
+
+    )
+
+
+    `)
+}
