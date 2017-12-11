0b80602373f19bf920918f611008b55551f6059a
Ivan Limongan <ivan.limongan@gmail.com>
2017-12-10 16:43:12 -0800

    database working, login and create account working

--- server.js
+++ server.js
@@ -3,12 +3,18 @@
 const pg = require('pg');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
-const constring = 'postgres://tanyagriego@localhost:5432/landinguser';
+const conString = 'postgres://ivanlimongan@localhost:5432/landinguser';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
 
 app.get('/', function(request, response) {
   response.sendFile('./public/index.html');
@@ -17,6 +23,46 @@ app.get('/', function(request, response) {
 app.listen(PORT, function() {
   console.log(`Listening on port:${PORT}`);
 });
+
+
+// INSERT RECORD
+
+app.post('/signup', function(request, response) {
+  client.query(
+    `INSERT INTO users (first_name, last_name, username, password)
+    VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING;`,
+    [request.body.first_name,
+    request.body.last_name,
+    request.body.username,
+    request.body.password]
+  )
+  .then(function() {
+    response.send('Insert Complete')
+  })
+  .catch(function(err) {
+    console.error(err)
+  });
+});
+
+app.post('/login', function(request, response) {
+  client.query(
+    `SELECT * FROM users 
+    WHERE username = $1 AND password = $2`,
+    [request.body.username,
+    request.body.password]
+  )
+  .then(function() {
+    response.send('Verified')
+  })
+  .catch(function(err) {
+    console.error(err)
+  });
+});
+
+
+
+
+// CREATE TABLES
 
 function loadDB() {
   client.query(`
@@ -28,7 +74,7 @@ function loadDB() {
       username VARCHAR(255),
       password VARCHAR(255)
     );`
-  )
+  );
   client.query(`
     CREATE TABLE IF NOT EXISTS
     styles (
@@ -37,10 +83,9 @@ function loadDB() {
       font_color VARCHAR(255),
       background_image VARCHAR(255),
       color_overlay VARCHAR(255),
-      gradient_overlay VARCHAR(255),
-
-    )
-
-
-    `)
+      gradient_overlay VARCHAR(255)
+    );`
+  );
 }
+
+loadDB();
