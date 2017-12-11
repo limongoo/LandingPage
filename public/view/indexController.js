9f0d486accd985b43b9eb5634219a02a074828e4
Langston McClarron <langstunned@gmail.com>
2017-12-10 16:05:40 -0800

    centered landing page image and text

--- public/view/indexController.js
+++ public/view/indexController.js
@@ -13,5 +13,5 @@ $('.randomColor').click(function(){
 $('.randomColorOverlay').click(function(){
   let colorRandom = '#' + ('00000' + (Math.random() * 16777216 << 0).toString(16)).substr(-6);
   console.log(colorRandom);
-  $('.randomImg').css('color', colorRandom);
+  $('.backgroundOutput', '.randomImg').css('background-color', colorRandom);
 });
