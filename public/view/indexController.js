d734996d2d61eab034851bee6dabb78fa4f6c602
tanyagriego <tanya.griego@gmail.com>
2017-12-10 16:10:15 -0800

    Added hide/view functionality to Font Style for index page.

--- public/view/indexController.js
+++ public/view/indexController.js
@@ -13,5 +13,17 @@ $('.randomColor').click(function(){
 $('.randomColorOverlay').click(function(){
   let colorRandom = '#' + ('00000' + (Math.random() * 16777216 << 0).toString(16)).substr(-6);
   console.log(colorRandom);
-  $('.randomImg').css('color', colorRandom);
+  $('.backgroundOutput', '.randomImg').css('background-color', colorRandom);
+});
+
+// Function to hide refresh icon if user clicks lock icon
+$('.lockStyle').on('click', function(event){
+  event.preventDefault();
+  $('.randomFont').hide();
+
+$('.lockStyle').on('click', function(event){
+  event.preventDefault();
+  $('.randomFont').show();
+})
+
 });
