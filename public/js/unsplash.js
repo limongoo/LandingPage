a7df1847fec2e81dccda867098667c2472e4766c
tanyagriego <tanya.griego@gmail.com>
2017-12-11 18:49:54 -0800

    Created DRY function to toggle between lock and unlock on index.html

--- public/js/unsplash.js
+++ public/js/unsplash.js
@@ -1,14 +1,14 @@
 // Unsplash API call
 // Credit: https://source.unsplash.com/  |  https://unsplash.com/
 
-$('.backgroundRandom').on('click', function() {
-    
+$('#randomBackground').on('click', function() {
+
     console.log('test')
-    
+
     // function getRandom() {
-        $.ajax({ 
+        $.ajax({
             method: 'GET',
-            url: 'https://api.unsplash.com/photos/random', 
+            url: 'https://api.unsplash.com/photos/random',
             headers: {'Authorization':`Client-ID ${token}`}
         }).done(function(results) {
             $('.randomImg').attr('src', results.urls.regular);
