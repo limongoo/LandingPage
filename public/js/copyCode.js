bb03907748d13a45cf226cf9b1ba74f4e62c0f39
tanyagriego <tanya.griego@gmail.com>
2017-12-11 20:50:10 -0800

    Added function to display copyCode info

--- public/js/copyCode.js
+++ public/js/copyCode.js
@@ -9,6 +9,24 @@ var modal = {};
   })
 };
 
+modal.displayCC = function () {
+const font = $('.fontStyleOutput').css('font-family');
+let message = `Google Font: ${font}<br>`;
+const fontColor = $('.fontStyleOutput').css('color');
+message += `Font Color: ${fontColor}<br>`;
+const backgroundColor = $('.backgroundOutput').css('background-color');
+message += `Background Image: ${backgroundColor}<br>`;
+$('#googleFontCC').show().text(message);
+};
+
+
+
+
+
+
+
+
 $(document).ready(function(){
   modal.copyCodeModal();
+  modal.displayCC();
 });
