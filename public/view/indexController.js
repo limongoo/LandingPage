ef8a640cf854a56053758938c120fe6a5c9644d0
Langston McClarron <langstunned@gmail.com>
2017-12-11 20:46:11 -0800

    achieved functions of gradient and color overlay and edited landing page css

--- public/view/indexController.js
+++ public/view/indexController.js
@@ -1,26 +1,41 @@
 // Randomization functionalities for the controller
+
+//Random Font
 $('#randomFont').click(function(){
   let fontRandom = fonts[Math.floor(Math.random() * fonts.length)];
   console.log(fontRandom);
   $('.fontStyleOutput').css('font-family', fontRandom);
 })
 
+//Random Color
 $('#randomColor').click(function(){
   let colorRandom = '#' + ('00000' + (Math.random() * 16777216 << 0).toString(16)).substr(-6);
   console.log(colorRandom);
   $('.fontStyleOutput, .fontColorOutput').css('color', colorRandom);
 });
 
+//Random RBGA Function for Color/Gradient Overlay usage
+function random_rgba() {
+  var o = Math.round, r = Math.random, s = 255;
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+//Random Color Overlay
 $('#randomColorOverlay').click(function(){
-  function random_rgba() {
-      var o = Math.round, r = Math.random, s = 255;
-      return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
-  }
+  
   let overlayRandom = random_rgba();
   $('.colorOverlayOutput').css('background', overlayRandom);
   console.log(overlayRandom);
 });
 
+//Random Gradient Overlay
+$('#randomGradientOverlay').click(function(){
+  let colorRandom = random_rgba();
+  let colorRandom2 = random_rgba();
+  $('.gradientOverlayOutput').css('background-image', `linear-gradient(${colorRandom},${colorRandom2})`);
+  console.log(colorRandom);
+  console.log(colorRandom2);
+})
 // Function to hide refresh icon if user clicks lock icon
 $('.lock').on('click', function(event){
   event.preventDefault();
