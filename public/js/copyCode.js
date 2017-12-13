6a4fc93cdc7fea62d00fe56515a6ac236e498fe7
tanyagriego <tanya.griego@gmail.com>
2017-12-12 19:53:38 -0800

    Modal window now displaying changing fonts, colors, etc.

--- public/js/copyCode.js
+++ public/js/copyCode.js
@@ -1,22 +1,39 @@
 'use strict';
 var modal = {};
 
-  modal.copyCodeModal = function () {
+modal.copyCodeModal = function () {
   $('#copyCodeIcon').on('click', function(event){
     event.preventDefault();
     $('.modal').addClass('is-visible');
     $('.modal.is-visible').show();
+    modal.displayCC();
   })
 };
 
 modal.displayCC = function () {
-const font = $('.fontStyleOutput').css('font-family');
-let message = `Google Font: ${font}<br>`;
-const fontColor = $('.fontStyleOutput').css('color');
-message += `Font Color: ${fontColor}<br>`;
-const backgroundColor = $('.backgroundOutput').css('background-color');
-message += `Background Image: ${backgroundColor}<br>`;
-$('#googleFontCC').show().text(message);
+  //font style//
+  const font = $('.fontStyleOutput').css('font-family');
+  let message = `Google Font: ${font}<br>`;
+
+  //font color//
+  const fontColor = $('.fontStyleOutput').css('color');
+  message += `Font Color: ${fontColor}<br>`;
+
+  //This is wrong. It's displaying a color, not the img- needs to be a link
+  const backgroundImage = $('.randomImg').attr('src');
+  message += `Background Image: ${backgroundImage}<br>`;
+
+  //color overlay//
+  const colorOverlay = $('.colorOverlayOutput').css('background');
+  message += `Color Overlay: ${colorOverlay}<br>`;
+
+  //gradient overlay//
+  const gradientOverlay = $('.gradientOverlayOutput').css('background-image');
+  message += `Gradient Overlay: ${gradientOverlay}<br>`;
+
+  //random gradient overlay//
+
+  $('#googleFontCC').show().html(message);
 };
 
 
@@ -28,5 +45,4 @@ $('#googleFontCC').show().text(message);
 
 $(document).ready(function(){
   modal.copyCodeModal();
-  modal.displayCC();
 });
