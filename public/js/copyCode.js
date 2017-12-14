ab19a0cb44f1c2e9af90e05234750241351816ca
tanyagriego <tanya.griego@gmail.com>
2017-12-13 19:42:47 -0800

    Brought Clear icon into modal window

--- public/js/copyCode.js
+++ public/js/copyCode.js
@@ -4,10 +4,10 @@ var modal = {};
 modal.copyCodeModal = function () {
   $('#copyCodeIcon').on('click', function(event){
     event.preventDefault();
+    console.log('Something else');
     $('.modal').addClass('is-visible');
     $('.modal.is-visible').show();
     modal.displayCC();
-    modal.hideModalOverlay();
     $('#clear').show();
   })
 };
@@ -15,36 +15,46 @@ modal.copyCodeModal = function () {
 modal.hideModalOverlay = function () {
   $('#clear').on('click', function(event) {
     event.preventDefault();
-    $('.modal').hide();
-    $('#clear').hide();
+    $('.modal').removeClass('is-visible');
+    // $('.modal').hide();
+    console.log('anything');
+    // $('#clear').hide();
   })
 };
 
 modal.displayCC = function () {
   //font style//
   const font = $('.fontStyleOutput').css('font-family');
-  let message = `Google Font: ${font}<br>`;
+  let message = `Google Font:<br>${font}<br><br>`;
 
   //font color//
   const fontColor = $('.fontStyleOutput').css('color');
-  message += `Font Color: ${fontColor}<br>`;
+  message += `Font Color:<br> ${fontColor}<br><br>`;
 
   //This is wrong. It's displaying a color, not the img- needs to be a link
   const backgroundImage = $('.randomImg').attr('src');
-  message += `Background Image: ${backgroundImage}<br>`;
+  message += `Background Image:<br> ${backgroundImage}<br><br>`;
 
   //color overlay//
   const colorOverlay = $('.colorOverlayOutput').css('background');
-  message += `Color Overlay: ${colorOverlay}<br>`;
+  message += `Color Overlay:<br> ${colorOverlay}<br><br>`;
 
   //gradient overlay//
   const gradientOverlay = $('.gradientOverlayOutput').css('background-image');
-  message += `Gradient Overlay: ${gradientOverlay}<br>`;
+  message += `Gradient Overlay:<br> ${gradientOverlay}<br><br>`;
 
   //random gradient overlay//
 
   $('#googleFontCC').show().html(message);
 };
+
+function copyToClipboard(element) {
+  var $copyCodeButton = $("<input>");
+  $(".modal").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
 
 
 
@@ -55,5 +65,5 @@ modal.displayCC = function () {
 
 $(document).ready(function(){
   modal.copyCodeModal();
-  // modal.hideModalOverlay();
+  modal.hideModalOverlay();
 });
